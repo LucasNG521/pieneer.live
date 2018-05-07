9ba153b725c704442e10ea63f9e3bae5fe4c88d7
ivanoung <ivanoung@gmail.com>
2018-05-07 17:23:19 +0800

    test case for chart action

---
+++ chartActions/chartMod.js
@@ -0,0 +1,39 @@
+class ChartDetails {
+    constructor(type, labels, nameOfTable, data = [], bgc = [], bdc = [], bdw = 1) {
+        this.types = type;
+        this.labels = labels;
+        this.labelOfTable = nameOfTable;
+        this.datas = data;
+        this.bgc = bgc;
+        this.bdc = bdc;
+        this.bdw = bdw;
+    }
+
+    // To output to Chart object
+    chartDetailInJson() {
+        return {
+            type: this.types,
+            data: {
+                labels: this.labels,
+                datasets: [{
+                    label: this.labelOfTable,
+                    data: this.datas,
+                    backgroundColor: this.bgc,
+                    borderColor: this.bdc,
+                    borderWidth: this.bdw
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        }
+    }
+}
+
+module.exports = ChartDetails;
