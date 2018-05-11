445e3e6b3fd133ccad51a4fffdc96b644fc480fd
ivanoung <ivanoung@gmail.com>
2018-05-11 11:19:23 +0800

    new migration - password type change, and create folder Class

--- databaseActions/folderActions.js
+++ databaseActions/folderActions.js
@@ -1,20 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 
+// Sync version is not working
+// Converting to promise later
+
 class FolderActions {
     constructor() {
         this.rootPath = path.join(__dirname, '/imageLibrary');
     }
 
-    mkdir(userid) {
-        fs.mkdir(path.join(this.rootPath, `user-${(userid.toString()).padStart(6,'0')}`), err => {
-            throw new Error('Folder is not creating');
+    mkUserDir(userid) {
+        fs.mkdirSync(path.join(this.rootPath, `/user-${(userid.toString()).padStart(6,'0')}`))
+    }
+    readUserDir(userid) {
+        fs.readdir(path.join(this.rootPath, `/user-${(userid.toString()).padStart(6,'0')}`), 'utf8', (err, nameArray) => {
+            if (err) {
+                throw new Error(err)
+            } else {
+                console.log(nameArray);
+            }
         })
     }
-    readdir(userid) {
-        fs.readdir(path.join(this.rootPath, `user-${(userid.toString()).padStart(6,'0')}`), err => {
-            throw new Error("Couldn't find folder");
-        })
+
+    mkPresentationDir() {
+
+    }
+    readPresentationDir() {
+
     }
 }
 
