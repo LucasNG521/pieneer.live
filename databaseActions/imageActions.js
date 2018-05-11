b6ddb8c724eba4ff7e923c27302c4ae04d0642bf
ivanoung <ivanoung@gmail.com>
2018-05-11 09:33:09 +0800

    changed send file method

--- databaseActions/imageActions.js
+++ databaseActions/imageActions.js
@@ -2,25 +2,43 @@ const fs = require('fs');
 const path = require('path');
 
 class ImageActions {
+
+
     readImage(req, res) {
+        const imageOptions = {
+            root: path.join(__dirname,
+                '/imageLibrary',
+                `/user-${req.params.userid}`,
+                `/presentation-${req.params.presentationid}`),
+            dotfiles: 'deny',
+            headers: {
+                'x-timestamp': Date.now(),
+                'x-sent': true
+            }
+        };
         if (req.query.pages) {
-            const img = fs
-                .readFile(path.join(__dirname,
-                        '/imageLibrary',
-                        `/user-${req.params.userid}`,
-                        `/presentation-${req.params.presentationid}`,
-                        `/${req.params.userid}-${req.params.presentationid}-${req.query.pages}.jpg`),
-                    (err, data) => {
-                        if (err) {
-                            throw new Error("WHAT THE FUCK IS THIS");
-                        } else {
-                            res.send(data);
-                        }
-                    });
+            res.sendFile(`${req.params.userid}-${req.params.presentationid}-${req.query.pages}.png`, imageOptions, function (err) {
+                if (err) {
+                    console.log(err);
+                    res.status(400).end();
+                } else {
+                    console.log('Image sent');
+                }
+            });
 
         }
 
     }
+    addImage(){
+        
+    }
+    removeImage(){
+
+    }
+    editImage(){
+
+    }
+    
 }
 
 module.exports = ImageActions;
