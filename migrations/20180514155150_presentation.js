48ee173ac7ca671f1af7ecef30cb64cf9fe6e0fd
LucasNG521 <ntw521@gmail.com>
2018-05-14 16:34:09 +0800

    new database (final version)

---
+++ migrations/20180514155150_presentation.js
@@ -0,0 +1,15 @@
+
+exports.up = function (knex, Promise) {
+  return knex.schema.createTable('presentation', (table) => {
+    table.increments();
+    table.integer('users_id').unsigned();
+    table.foreign('users_id').references('users.id');
+    table.string('location');
+    table.string('date');
+    table.timestamps(false, true);
+  });
+};
+
+exports.down = function (knex, Promise) {
+  return knex.schema.dropTable('presentation');
+};
